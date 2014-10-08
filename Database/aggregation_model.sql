04174fb3c8d1e119d067537d53fb1f9905128786
Luis <ayoria@gmail.com>
2014-10-08 20:23:28 +0200

    Update Schema dB and creation tables

--- Database/aggregation_model.sql
+++ Database/aggregation_model.sql
@@ -1,193 +1,284 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+-- MySQL dump 10.13  Distrib 5.1.69, for redhat-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: kpis_db
+-- ------------------------------------------------------
+-- Server version	5.1.69-log
 
-DROP SCHEMA IF EXISTS `kpis_db` ;
-CREATE SCHEMA IF NOT EXISTS `kpis_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci ;
-USE `kpis_db` ;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Table `kpis_db`.`alias_ip_servidores_aplicaciones`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `kpis_db`.`alias_ip_servidores_aplicaciones` ;
+--
+-- Table structure for table `alias_ip_servidores_aplicaciones`
+--
 
-CREATE TABLE IF NOT EXISTS `kpis_db`.`alias_ip_servidores_aplicaciones` (
-  `ip` VARCHAR(20) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NOT NULL,
-  `alias` VARCHAR(20) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL,
-  `country` VARCHAR(20) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL,
-  `continent` VARCHAR(20) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL,
-  `isp` VARCHAR(20) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL,
-  PRIMARY KEY (`ip`))
-ENGINE = MyISAM
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_spanish_ci;
+DROP TABLE IF EXISTS `alias_ip_servidores_aplicaciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alias_ip_servidores_aplicaciones` (
+  `ip` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `alias` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `country` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `continent` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `isp` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
+  PRIMARY KEY (`ip`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `calendar`
+--
 
--- -----------------------------------------------------
--- Table `kpis_db`.`operator`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `kpis_db`.`operator` ;
+DROP TABLE IF EXISTS `calendar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `calendar` (
+  `calendar_datetime` datetime NOT NULL,
+  PRIMARY KEY (`calendar_datetime`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `kpis_db`.`operator` (
-  `idoperator` INT(11) NOT NULL AUTO_INCREMENT,
-  `alias` VARCHAR(150) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL,
-  PRIMARY KEY (`idoperator`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 1217
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_spanish_ci;
+--
+-- Table structure for table `network_country_codes`
+--
 
+DROP TABLE IF EXISTS `network_country_codes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `network_country_codes` (
+  `mcc` char(3) COLLATE utf8_spanish_ci NOT NULL,
+  `mnc` char(3) COLLATE utf8_spanish_ci NOT NULL,
+  `country` char(3) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `ob` char(150) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `alias` int(11) DEFAULT NULL,
+  PRIMARY KEY (`mcc`,`mnc`),
+  KEY `fk_network_country_codes_operator1_idx` (`alias`),
+  CONSTRAINT `fk_network_country_codes_operator1` FOREIGN KEY (`alias`) REFERENCES `operator` (`idoperator`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `kpis_db`.`network_country_codes`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `kpis_db`.`network_country_codes` ;
+--
+-- Table structure for table `notificaciones_entrantes_agr`
+--
 
-CREATE TABLE IF NOT EXISTS `kpis_db`.`network_country_codes` (
-  `mcc` CHAR(3) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NOT NULL,
-  `mnc` CHAR(3) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NOT NULL,
-  `country` CHAR(3) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL,
-  `ob` CHAR(150) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL,
-  `alias` INT(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`mcc`, `mnc`),
-  INDEX `fk_network_country_codes_operator1_idx` (`alias` ASC),
-  CONSTRAINT `fk_network_country_codes_operator1`
-    FOREIGN KEY (`alias`)
-    REFERENCES `kpis_db`.`operator` (`idoperator`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_spanish_ci;
+DROP TABLE IF EXISTS `notificaciones_entrantes_agr`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `notificaciones_entrantes_agr` (
+  `fecha` datetime NOT NULL COMMENT 'viene de notificaciones_entrantes',
+  `ip` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `total` int(10) unsigned DEFAULT NULL,
+  KEY `fecha` (`fecha`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `notificaciones_entrantes_agr_ld`
+--
 
--- -----------------------------------------------------
--- Table `kpis_db`.`notificaciones_entrantes_agr`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `kpis_db`.`notificaciones_entrantes_agr` ;
+DROP TABLE IF EXISTS `notificaciones_entrantes_agr_ld`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `notificaciones_entrantes_agr_ld` (
+  `fecha` datetime NOT NULL COMMENT 'viene de notificaciones_entrantes',
+  `ip` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `total` int(10) unsigned DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `kpis_db`.`notificaciones_entrantes_agr` (
-  `fecha` DATETIME NOT NULL COMMENT 'viene de notificaciones_entrantes',
-  `ip` VARCHAR(20) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NOT NULL,
-  `total` INT(10) UNSIGNED NULL DEFAULT NULL,
-  INDEX `fecha` (`fecha` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_spanish_ci;
+--
+-- Table structure for table `notificaciones_entrantes_ncc_agr`
+--
 
+DROP TABLE IF EXISTS `notificaciones_entrantes_ncc_agr`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `notificaciones_entrantes_ncc_agr` (
+  `fecha` datetime NOT NULL,
+  `mcc` varchar(3) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `mnc` varchar(3) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `total` int(10) unsigned DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `kpis_db`.`notificaciones_entrantes_ncc_agr`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `kpis_db`.`notificaciones_entrantes_ncc_agr` ;
+--
+-- Table structure for table `notificaciones_entrantes_ncc_agr_ld`
+--
 
-CREATE TABLE IF NOT EXISTS `kpis_db`.`notificaciones_entrantes_ncc_agr` (
-  `fecha` DATETIME NOT NULL,
-  `mcc` VARCHAR(3) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL,
-  `mnc` VARCHAR(3) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL,
-  `total` INT(10) UNSIGNED NULL DEFAULT NULL)
-ENGINE = MyISAM
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_spanish_ci
-COMMENT = 'quizas añadir IP';
+DROP TABLE IF EXISTS `notificaciones_entrantes_ncc_agr_ld`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `notificaciones_entrantes_ncc_agr_ld` (
+  `fecha` datetime NOT NULL,
+  `mcc` varchar(3) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `mnc` varchar(3) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `total` int(10) unsigned DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `notificaciones_entregadas_agr`
+--
 
--- -----------------------------------------------------
--- Table `kpis_db`.`notificaciones_entregadas_agr`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `kpis_db`.`notificaciones_entregadas_agr` ;
+DROP TABLE IF EXISTS `notificaciones_entregadas_agr`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `notificaciones_entregadas_agr` (
+  `fecha` datetime NOT NULL,
+  `mcc` varchar(3) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `mnc` varchar(3) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `total` int(10) unsigned DEFAULT NULL COMMENT 'total viene de notificaciones_ws',
+  KEY `fecha` (`fecha`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `kpis_db`.`notificaciones_entregadas_agr` (
-  `fecha` DATETIME NOT NULL,
-  `mcc` VARCHAR(3) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL,
-  `mnc` VARCHAR(3) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL,
-  `total` INT(10) UNSIGNED NULL DEFAULT NULL COMMENT 'total viene de notificaciones_ws',
-  INDEX `fecha` (`fecha` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_spanish_ci;
+--
+-- Table structure for table `notificaciones_enviadas_wu`
+--
 
+DROP TABLE IF EXISTS `notificaciones_enviadas_wu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `notificaciones_enviadas_wu` (
+  `fecha` datetime NOT NULL COMMENT 'Viene de notificaciones_udp',
+  `mcc` varchar(3) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `mnc` varchar(3) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `wakeup` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `total` int(11) DEFAULT NULL,
+  KEY `fecha` (`fecha`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `kpis_db`.`notificaciones_enviadas_wu`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `kpis_db`.`notificaciones_enviadas_wu` ;
+--
+-- Table structure for table `operator`
+--
 
-CREATE TABLE IF NOT EXISTS `kpis_db`.`notificaciones_enviadas_wu` (
-  `fecha` DATETIME NOT NULL COMMENT 'Viene de notificaciones_udp',
-  `mcc` VARCHAR(3) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL,
-  `mnc` VARCHAR(3) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL,
-  `wakeup` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL,
-  `total` INT(11) NULL DEFAULT NULL,
-  INDEX `fecha` (`fecha` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_spanish_ci;
+DROP TABLE IF EXISTS `operator`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `operator` (
+  `idoperator` int(11) NOT NULL AUTO_INCREMENT,
+  `alias` varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
+  PRIMARY KEY (`idoperator`)
+) ENGINE=InnoDB AUTO_INCREMENT=1225 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `peticiones_registros_agr`
+--
 
--- -----------------------------------------------------
--- Table `kpis_db`.`peticiones_registros_agr`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `kpis_db`.`peticiones_registros_agr` ;
+DROP TABLE IF EXISTS `peticiones_registros_agr`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `peticiones_registros_agr` (
+  `fecha` datetime NOT NULL COMMENT 'viene de peticiones_registros',
+  `mcc` varchar(3) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `mnc` varchar(3) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `total` int(11) unsigned DEFAULT NULL,
+  KEY `fehca` (`fecha`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `kpis_db`.`peticiones_registros_agr` (
-  `fecha` DATETIME NOT NULL COMMENT 'viene de peticiones_registros',
-  `mcc` VARCHAR(3) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL,
-  `mnc` VARCHAR(3) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL,
-  `total` INT(11) UNSIGNED NULL DEFAULT NULL,
-  INDEX `fehca` (`fecha` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_spanish_ci;
+--
+-- Table structure for table `terminales_agr`
+--
 
+DROP TABLE IF EXISTS `terminales_agr`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `terminales_agr` (
+  `fecha` datetime NOT NULL,
+  `registrados` int(10) DEFAULT NULL,
+  `activos` int(10) DEFAULT NULL,
+  `conectados` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `kpis_db`.`terminales_agr`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `kpis_db`.`terminales_agr` ;
+--
+-- Table structure for table `terminales_agr_ld`
+--
 
-CREATE TABLE IF NOT EXISTS `kpis_db`.`terminales_agr` (
-  `fecha` DATETIME NOT NULL,
-  `registrados` INT(10) UNSIGNED NULL DEFAULT NULL,
-  `activos` INT(10) UNSIGNED NULL DEFAULT NULL,
-  `conectados` INT(10) UNSIGNED NULL DEFAULT NULL)
-ENGINE = MyISAM
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_spanish_ci;
+DROP TABLE IF EXISTS `terminales_agr_ld`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `terminales_agr_ld` (
+  `fecha` datetime NOT NULL,
+  `activos` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `terminales_agr_m`
+--
 
--- -----------------------------------------------------
--- Table `kpis_db`.`terminales_con_registros_agr`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `kpis_db`.`terminales_con_registros_agr` ;
+DROP TABLE IF EXISTS `terminales_agr_m`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `terminales_agr_m` (
+  `fecha` datetime NOT NULL,
+  `activos` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `kpis_db`.`terminales_con_registros_agr` (
-  `fecha` DATETIME NOT NULL,
-  `mcc` VARCHAR(3) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL,
-  `mnc` VARCHAR(3) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL,
-  `total` INT(11) NULL DEFAULT NULL,
-  INDEX `fecha` (`fecha` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_spanish_ci;
+--
+-- Table structure for table `terminales_agr_w`
+--
 
+DROP TABLE IF EXISTS `terminales_agr_w`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `terminales_agr_w` (
+  `fecha` datetime NOT NULL,
+  `activos` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `kpis_db`.`tiempo_notificaciones_agr`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `kpis_db`.`tiempo_notificaciones_agr` ;
+--
+-- Table structure for table `terminales_con_registros_agr`
+--
 
-CREATE TABLE IF NOT EXISTS `kpis_db`.`tiempo_notificaciones_agr` (
-  `fecha` DATETIME NOT NULL,
-  `max` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL,
-  `min` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL,
-  `avg` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NULL DEFAULT NULL COMMENT 'La media se puede calcular en periodos mayores ponderando por el número de notificaciones que está en notificaciones_entregadas_agr',
-  INDEX `fecha` (`fecha` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_spanish_ci;
+DROP TABLE IF EXISTS `terminales_con_registros_agr`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `terminales_con_registros_agr` (
+  `fecha` datetime NOT NULL,
+  `mcc` varchar(3) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `mnc` varchar(3) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `total` int(11) DEFAULT NULL,
+  KEY `fecha` (`fecha`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `tiempo_notificaciones_agr`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+DROP TABLE IF EXISTS `tiempo_notificaciones_agr`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tiempo_notificaciones_agr` (
+  `fecha` datetime NOT NULL,
+  `max` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `min` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `avg` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL COMMENT 'La media se puede calcular en periodos mayores ponderando por el nÃºmero de notificaciones que estÃ¡ en notificaciones_entregadas_agr',
+  KEY `fecha` (`fecha`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-10-08 18:38:43
